8a2323f6da381681293fcba5cf73397587145d37
gugahnstn <gustavonstn@hotmail.com>
2023-09-11 16:30:33 -0300

    Initial commit

---
+++ app/routes/posts._index.tsx
@@ -0,0 +1,30 @@
+import { useLoaderData } from "@remix-run/react";
+import { Link } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { getPosts } from "./posts.server";
+
+export const loader = async () => {
+  return json({posts: await getPosts()});
+}
+
+export default function Posts() {
+  const { posts } = useLoaderData<typeof loader>();
+
+  return (
+    <main>
+      <h1> Hello world!</h1>
+      <ul>
+        {posts.map((post) => (
+           <li key={post.slug}>
+           <Link
+             to={post.slug}
+             className="text-blue-600 underline"
+           >
+             {post.title}
+           </Link>
+         </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
